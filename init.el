0423a7e14e759551fc408d4aedb04195ab17a8cc
Malk’Zameth <m@zameth.org>
2013-01-09 19:11:37 +0100

    added ace jump mode

--- init.el
+++ init.el
@@ -74,6 +74,29 @@
       )
 (require 'ffap)
 
+;;
+;; ace jump mode major function
+;; 
+(autoload
+  'ace-jump-mode
+  "ace-jump-mode"
+  "Emacs quick move minor mode"
+  t)
+(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
+
+;; 
+;; enable a more powerful jump back function from ace jump mode
+;;
+(autoload
+  'ace-jump-mode-pop-mark
+  "ace-jump-mode"
+  "Ace jump back:-)"
+  t)
+(eval-after-load "ace-jump-mode"
+  '(ace-jump-mode-enable-mark-sync))
+(define-key global-map (kbd "C-x SPC") 'ace-jump-mode-pop-mark)
+
+
 ;; Dev conf
 (require 'idle-highlight-mode)
 (defun my-coding-hook ()
