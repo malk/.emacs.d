5d60e7cd08a86f4c0f4ed75245f42302db256e4a
Malk’Zameth <m@zameth.org>
2013-01-09 19:12:00 +0100

    added erc support

--- init.el
+++ init.el
@@ -110,7 +110,6 @@
 ;; version control and backup
 (require 'magit)
 
-
 ;; session
 (require 'saveplace)
 (setq save-place t)
@@ -118,6 +117,32 @@
 ;; org-mode
 (setq org-icalendar-store-UID t
       org-icalendar-include-todo t)
+
+
+;; ERC
+(require 'erc)
+(erc-autojoin-mode t)
+(setq erc-autojoin-channels-alist
+  '((".*\\.freenode.net" "#emacs" "#linagora")))
+;; check channels
+(erc-track-mode t)
+(setq erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT" "MODE"
+                                 "324" "329" "332" "333" "353" "477"))
+;; don't show any of this
+(setq erc-hide-list '("JOIN" "PART" "QUIT" "NICK"))
+
+(defun erc-start-or-switch ()
+  "Connect to ERC, or switch to last active buffer"
+  (interactive)
+  (if (get-buffer "irc.freenode.net:6667") ;; ERC already active?
+
+    (erc-track-switch-buffer 1) ;; yes: switch to last active
+    (when (y-or-n-p "Start ERC? ") ;; no: maybe start ERC
+      (erc :server "irc.freenode.net" :port 6667 :nick "Malk_Zameth" :full-name "Malk'Zameth")
+      (erc :server "localhost" :port 6667 :nick "malk" :full-name "malk"))))
+
+;; switch to ERC with Ctrl+c e
+(global-set-key [f12] 'erc-start-or-switch)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
