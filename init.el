e0c4cb9a9fef89b495d414d42791f0bc94049b17
Malk’Zameth <m@zameth.org>
2013-01-14 12:58:02 +0100

    Added ignore-whitespace support to magit

--- init.el
+++ init.el
@@ -286,6 +286,25 @@
 
 (define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
 
+;; Make magit switch between ignoring whitespace or not ignoring
+(defun magit-toggle-whitespace ()
+  (interactive)
+  (if (member "-w" magit-diff-options)
+      (magit-dont-ignore-whitespace)
+    (magit-ignore-whitespace)))
+
+(defun magit-ignore-whitespace ()
+  (interactive)
+  (add-to-list 'magit-diff-options "-w")
+  (magit-refresh))
+
+(defun magit-dont-ignore-whitespace ()
+  (interactive)
+  (setq magit-diff-options (remove "-w" magit-diff-options))
+  (magit-refresh))
+
+(define-key magit-status-mode-map (kbd "W") 'magit-toggle-whitespace)
+
 ;; Auto refresh buffers, so when we change brancheswwe have the desired
 ;; version of the file (or when the file changes on diskand we did not
 ;; notice)
